cedbeb7541808b6f4d6546d3eaafea4950dae495
huangdengfeng <734839030@qq.com>
2024-01-29 15:15:59 +0800

    fix: route is null

--- src/router/index.ts
+++ src/router/index.ts
@@ -60,8 +60,7 @@ export const getRoutesExpanded = () => {
 
 export const getActive = (maxLevel = 3): string => {
   const route = useRoute();
-
-  if (!route.path) {
+  if (!route?.path) {
     return '';
   }
 
